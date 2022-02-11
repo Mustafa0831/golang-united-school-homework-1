240b71dda504d8cf5b94b3b4feb9a8a10b941591
Mustafa Ustaz <mr.mus0831@gmail.com>
2022-02-11 23:03:54 +0600

    task2

--- 00_test.go
+++ 00_test.go
@@ -9,7 +9,7 @@ func Test0(t *testing.T) {
 	want := string([]rune{72, 101, 108, 108, 111, 32, 128506, 65039, 32, 33})
 	check:=PrintS()
 	if!strings.EqualFold(want,check) {
-		t.Errorf("Result got was incorrect, check: %s, want: %s", want, "Hello :world_map:!")
+		t.Errorf("Result got was incorrect, check: %v, want: %v", want, "Hello :world_map:!")
 
 	}
 }
