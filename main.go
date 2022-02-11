1d319a131dd6335d9d0848903b8415f3109e1c9e
Mustafa Ustaz <mr.mus0831@gmail.com>
2022-02-11 23:28:56 +0600

    task2

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 }
 
 func PrintS() string {
-	fmt.Println("Hello 🗺️!")
+	fmt.Println("Hello 🗺️ !")
 	// want := string([]rune{72, 101, 108, 108, 111, 32, 128506, 65039, 32, 33})
 
 	return emoji.Sprintf("Hello :world_map: !")
