86e2289d1a6032da2e384a6265a3a8064d7afe61
Mustafa Ustaz <mr.mus0831@gmail.com>
2022-02-11 23:07:37 +0600

    task2

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/kyokomi/emoji"
 )
 
 func main() {
 	PrintS()
+	// fmt.Println("ss")
 }
 
 func PrintS() string {
-	//  fmt.Println("Hello 🗺️!")
+	 fmt.Println("Hello 🗺️!")
 	// want := string([]rune{72, 101, 108, 108, 111, 32, 128506, 65039, 32, 33})
 
 	return emoji.Sprintf("Hello :world_map:!")
